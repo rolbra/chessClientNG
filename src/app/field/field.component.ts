4b5eed636f976c64fc06a0c949d9e5edf281af89
rolbra <roli_brandmeister@web.de>
2025-02-17 09:27:28 +0100

    prepare mapping for style and content

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-field',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './field.component.html',
   styleUrl: './field.component.scss'
 })
 export class FieldComponent {
-
+  code: string = '&#x265F';
+  accessable: boolean = true;
 }
